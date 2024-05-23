c341f2dc16f615a130d57fe08ae653cd7a3a022e
Danis <danis.ramirez.hn@gmail.com>
2024-05-23 23:57:44 +0200

    UI improvement

--- lib/screens/training_screen.dart
+++ lib/screens/training_screen.dart
@@ -13,25 +13,64 @@ class TrainingScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Training'),
+        centerTitle: true,
       ),
       body: Consumer<WorkoutModel>(
         builder: (context, workoutModel, child) {
-          return ListView.builder(
-            itemCount: workoutModel.routines.length,
-            itemBuilder: (context, index) {
-              final routine = workoutModel.routines[index];
-              return ListTile(
-                title: Text(routine.name),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => StartRoutineScreen(routine: routine),
-                    ),
-                  );
-                },
-              );
-            },
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: workoutModel.routines.isNotEmpty
+                ? ListView.builder(
+              itemCount: workoutModel.routines.length,
+              itemBuilder: (context, index) {
+                final routine = workoutModel.routines[index];
+                return RoutineCard(routine: routine);
+              },
+            )
+                : Center(
+              child: Text(
+                'No routines created yet.',
+                style: TextStyle(fontSize: 18.0, color: Colors.grey[600]),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class RoutineCard extends StatelessWidget {
+  final Routine routine;
+
+  const RoutineCard({required this.routine});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+      elevation: 5,
+      child: ListTile(
+        contentPadding: const EdgeInsets.all(16.0),
+        title: Text(
+          routine.name,
+          style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text(
+          '${routine.exercises.length} exercises',
+          style: TextStyle(color: Colors.grey[700]),
+        ),
+        trailing: Icon(
+          Icons.arrow_forward_ios,
+          color: Theme.of(context).primaryColor,
+        ),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => StartRoutineScreen(routine: routine),
+            ),
           );
         },
       ),
