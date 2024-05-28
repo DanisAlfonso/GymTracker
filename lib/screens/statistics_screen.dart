e0b133521d85d393669678d1dfe2ccbb01d57925
Danis <danis.ramirez.hn@gmail.com>
2024-05-28 15:00:07 +0200

    improve UI Statistics Screen

--- lib/screens/statistics_screen.dart
+++ lib/screens/statistics_screen.dart
@@ -28,51 +28,91 @@ class _StatisticsScreenState extends State<StatisticsScreen> {
           builder: (context, workoutModel, child) {
             final exercises = workoutModel.exercises;
 
-            return SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'Select Exercise',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            // Sort exercises alphabetically
+            exercises.sort((a, b) => a.name.compareTo(b.name));
+
+            return ListView(
+              children: [
+                const Text(
+                  'Select Exercise',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 16),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).inputDecorationTheme.fillColor,
+                    borderRadius: BorderRadius.circular(8.0),
+                    border: Border.all(color: Colors.grey.shade300),
                   ),
-                  const SizedBox(height: 16),
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).inputDecorationTheme.fillColor,
-                      borderRadius: BorderRadius.circular(8.0),
-                      border: Border.all(color: Colors.grey.shade300),
-                    ),
-                    child: DropdownButtonHideUnderline(
-                      child: DropdownButton<Exercise>(
-                        hint: const Text('Select Exercise'),
-                        value: _selectedExercise,
-                        onChanged: (Exercise? newValue) {
-                          setState(() {
-                            _selectedExercise = newValue;
-                          });
-                        },
-                        items: exercises.map((Exercise exercise) {
-                          return DropdownMenuItem<Exercise>(
-                            value: exercise,
-                            child: Text(exercise.name),
-                          );
-                        }).toList(),
-                      ),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton<Exercise>(
+                      isDense: true,
+                      hint: const Text('Select Exercise'),
+                      value: _selectedExercise,
+                      onChanged: (Exercise? newValue) {
+                        setState(() {
+                          _selectedExercise = newValue;
+                        });
+                      },
+                      items: exercises.map((Exercise exercise) {
+                        return DropdownMenuItem<Exercise>(
+                          value: exercise,
+                          child: Text(
+                            exercise.name,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        );
+                      }).toList(),
                     ),
                   ),
-                  const SizedBox(height: 32),
-                  if (_selectedExercise != null)
-                    ExercisePerformanceSection(selectedExercise: _selectedExercise!),
-                  const SizedBox(height: 32),
-                  WeeklyProgressSection(),
-                  const SizedBox(height: 32),
-                  ExerciseFrequencySection(),
-                  const SizedBox(height: 32),
-                  MonthlyProgressSection(),
-                ],
-              ),
+                ),
+                const SizedBox(height: 32),
+                if (_selectedExercise != null)
+                  Card(
+                    elevation: 4.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: ExercisePerformanceSection(selectedExercise: _selectedExercise!),
+                    ),
+                  ),
+                const SizedBox(height: 32),
+                Card(
+                  elevation: 4.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: WeeklyProgressSection(),
+                  ),
+                ),
+                const SizedBox(height: 32),
+                Card(
+                  elevation: 4.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: ExerciseFrequencySection(),
+                  ),
+                ),
+                const SizedBox(height: 32),
+                Card(
+                  elevation: 4.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: MonthlyProgressSection(),
+                  ),
+                ),
+              ],
             );
           },
         ),
