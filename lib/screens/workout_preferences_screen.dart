96e1fffa7f708637d6f92f8cd244eebd148a7dea
Danis <danis.ramirez.hn@gmail.com>
2024-06-05 21:44:48 +0200

    with cards and ensuring the button text is always visible, regardless of light or dark mode

--- lib/screens/workout_preferences_screen.dart
+++ lib/screens/workout_preferences_screen.dart
@@ -59,79 +59,134 @@ class _WorkoutPreferencesScreenState extends State<WorkoutPreferencesScreen> {
           key: _formKey,
           child: ListView(
             children: [
-              SwitchListTile(
-                title: Text(appLocalizations?.translate('use_kg') ?? 'Use kg'),
+              _buildSwitchListTile(
+                title: appLocalizations?.translate('use_kg') ?? 'Use kg',
                 value: _useKg,
-                onChanged: (bool value) {
+                onChanged: (value) {
                   setState(() {
                     _useKg = value;
                   });
                 },
               ),
               const SizedBox(height: 20),
-              Text(
-                appLocalizations?.translate('default_rest_time') ?? 'Default Rest Time',
-                style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: TextFormField(
-                      decoration: InputDecoration(labelText: appLocalizations?.translate('minutes') ?? 'Minutes'),
-                      keyboardType: TextInputType.number,
-                      initialValue: _defaultRestMinutes.toString(),
-                      onChanged: (value) {
-                        setState(() {
-                          _defaultRestMinutes = int.parse(value);
-                        });
-                      },
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return appLocalizations?.translate('please_enter_minutes') ?? 'Please enter minutes';
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                  const SizedBox(width: 10),
-                  Expanded(
-                    child: TextFormField(
-                      decoration: InputDecoration(labelText: appLocalizations?.translate('seconds') ?? 'Seconds'),
-                      keyboardType: TextInputType.number,
-                      initialValue: _defaultRestSeconds.toString(),
-                      onChanged: (value) {
-                        setState(() {
-                          _defaultRestSeconds = int.parse(value);
-                        });
-                      },
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return appLocalizations?.translate('please_enter_seconds') ?? 'Please enter seconds';
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                ],
-              ),
+              _buildSectionHeader(appLocalizations?.translate('default_rest_time') ?? 'Default Rest Time'),
+              const SizedBox(height: 10),
+              _buildTimePickerRow(context),
               const SizedBox(height: 20),
-              SwitchListTile(
-                title: Text(appLocalizations?.translate('enable_workout_reminders') ?? 'Enable workout reminders'),
+              _buildSwitchListTile(
+                title: appLocalizations?.translate('enable_workout_reminders') ?? 'Enable workout reminders',
                 value: _workoutReminders,
-                onChanged: (bool value) {
+                onChanged: (value) {
                   setState(() {
                     _workoutReminders = value;
                   });
                 },
               ),
               const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _savePreferences,
-                child: Text(appLocalizations?.translate('save_preferences') ?? 'Save Preferences'),
+              Center(
+                child: ElevatedButton(
+                  onPressed: _savePreferences,
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    backgroundColor: Theme.of(context).primaryColor,
+                    textStyle: TextStyle(
+                      color: Theme.of(context).brightness == Brightness.dark ? Colors.white : Colors.black,
+                    ),
+                  ),
+                  child: Text(
+                    appLocalizations?.translate('save_preferences') ?? 'Save Preferences',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSwitchListTile({required String title, required bool value, required ValueChanged<bool> onChanged}) {
+    return Card(
+      elevation: 2,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      child: SwitchListTile(
+        title: Text(title),
+        value: value,
+        onChanged: onChanged,
+      ),
+    );
+  }
+
+  Widget _buildSectionHeader(String title) {
+    return Text(
+      title,
+      style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+    );
+  }
+
+  Widget _buildTimePickerRow(BuildContext context) {
+    final appLocalizations = AppLocalizations.of(context);
+
+    return Card(
+      elevation: 2,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: appLocalizations?.translate('minutes') ?? 'Minutes',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+                keyboardType: TextInputType.number,
+                initialValue: _defaultRestMinutes.toString(),
+                onChanged: (value) {
+                  setState(() {
+                    _defaultRestMinutes = int.parse(value);
+                  });
+                },
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return appLocalizations?.translate('please_enter_minutes') ?? 'Please enter minutes';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: appLocalizations?.translate('seconds') ?? 'Seconds',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+                keyboardType: TextInputType.number,
+                initialValue: _defaultRestSeconds.toString(),
+                onChanged: (value) {
+                  setState(() {
+                    _defaultRestSeconds = int.parse(value);
+                  });
+                },
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return appLocalizations?.translate('please_enter_seconds') ?? 'Please enter seconds';
+                  }
+                  return null;
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
