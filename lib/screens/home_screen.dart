71bae5adb0b988518740b7658dd81c934aeaa914
Danis <danis.ramirez.hn@gmail.com>
2024-06-19 22:59:11 +0200

    Remove Recent Activities from Home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+// lib/screens/home_screen.dart
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
@@ -13,7 +14,6 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final appLocalizations = AppLocalizations.of(context);
     final workoutModel = Provider.of<WorkoutModel>(context);
-    final recentWorkouts = workoutModel.workouts.take(5).toList();
     final theme = Theme.of(context);
     final cardBorder = BorderSide(color: theme.dividerColor.withOpacity(0.5));
 
@@ -32,69 +32,7 @@ class HomeScreen extends StatelessWidget {
             const SizedBox(height: 20),
             const RecoveryStatus(), // Add the RecoveryStatus widget
             const SizedBox(height: 20),
-            _buildRecentActivities(context, recentWorkouts, cardBorder),
-            const SizedBox(height: 20),
             _buildAllActivities(context, workoutModel.workouts, cardBorder),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildRecentActivities(BuildContext context, List<Workout> recentWorkouts, BorderSide cardBorder) {
-    final appLocalizations = AppLocalizations.of(context);
-    final theme = Theme.of(context);
-    final isDarkMode = theme.brightness == Brightness.dark;
-    final iconColor = isDarkMode ? Colors.white : theme.primaryColor;
-    final textColor = isDarkMode ? Colors.white : Colors.black;
-
-    Map<String, List<Workout>> groupedWorkouts = {};
-
-    for (var workout in recentWorkouts) {
-      String categoryKey = "${workout.exercise.localizationKey}_description";
-      String category = appLocalizations?.translate(categoryKey) ?? workout.exercise.description;
-      if (groupedWorkouts[category] == null) {
-        groupedWorkouts[category] = [];
-      }
-      groupedWorkouts[category]!.add(workout);
-    }
-
-    return Card(
-      elevation: 4,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15),
-        side: cardBorder,
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                Icon(Icons.history, color: iconColor), // Adjusted icon color based on theme
-                const SizedBox(width: 8),
-                Text(
-                  appLocalizations?.translate('recent_activities') ?? 'Recent Activities',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: textColor),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            ...groupedWorkouts.entries.map((entry) {
-              return ExpansionTile(
-                title: Text(
-                  entry.key,
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: textColor),
-                ),
-                children: [_buildWorkoutDataTable(context, entry.value)],
-              );
-            }),
-            if (recentWorkouts.isEmpty)
-              Text(
-                appLocalizations?.translate('no_recent_activities') ?? 'No recent activities',
-                style: const TextStyle(fontSize: 16, color: Colors.grey),
-              ),
           ],
         ),
       ),
